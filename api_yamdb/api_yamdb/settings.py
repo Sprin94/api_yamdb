75488fbaef3076d922f511f5d9a175ab91c2352c
Sprin <Electron4ik123@gmai.com>
2022-12-27 08:23:50 +0800

    Добавил модель User

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-# Application definition
+AUTH_USER_MODEL = 'users.User'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -102,3 +102,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+
+if os.getenv('EMAIL_HOST'):
+    EMAIL_HOST = os.getenv('EMAIL_HOST')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+    DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+    EMAIL_PORT = os.getenv('EMAIL_PORT')
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,1 +1,52 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.core.exceptions import ValidationError
+
+
+def generate_uuid(max_length=12) -> str:
+    """Генерирует UUID"""
+    return uuid.uuid4().hex[:max_length]
+
+
+def username_not_me(username):
+    if username == 'me':
+        raise ValidationError('username не может быть \"me\"')
+
+
+class User(AbstractUser):
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        validators=[username_validator, username_not_me],
+    )
+    email = models.EmailField(
+        verbose_name='Email',
+        unique=True,
+        max_length=254,
+
+    )
+    confirmation_code = models.CharField(
+        verbose_name='Код подтверждения',
+        max_length=50,
+        unique=True,
+        default=generate_uuid,
+    )
+    bio = models.TextField(
+        verbose_name='Био',
+        blank=True,
+    )
+
+    class RoleChoice(models.TextChoices):
+        USER = 1, 'Пользователь'
+        MODERATOR = 2, 'Модератор'
+        ADMIN = 3, 'Администратор'
+
+    role = models.IntegerField(
+        verbose_name='Роль',
+        choices=RoleChoice.choices,
+        default=RoleChoice.USER
+    )
