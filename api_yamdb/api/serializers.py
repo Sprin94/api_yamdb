1a61f91580213773ca4c0705103af8f566eff8a6
Яна <yuki122@yandex.ru>
2022-12-28 15:22:26 +0500

    Добавлена проверка на отзыв

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,11 +1,26 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.generics import get_object_or_404
 
-from reviews.models import Comment, Review
+from reviews.models import Comment, Review, Title
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    title = serializers.SlugRelatedField(
+        read_only=True, slug_field='name')
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username')
+
+    def validate(self, data):
+        request = self.context['request']
+        title = get_object_or_404(
+            Title, pk=self.context['view'].kwargs.get('title_id'))
+        if request.method == 'POST':
+            if Review.objects.filter(title=title,
+                                     author=request.user).exists():
+                raise ValidationError('Нельзя добавить больше одного'
+                                      'отзыва на произведение')
+        return data
 
     class Meta:
         fields = '__all__'
