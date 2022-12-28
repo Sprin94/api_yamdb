f0a91fd86939efbc93ddb8f2815f15c47dd19c6c
Sprin <Electron4ik123@gmai.com>
2022-12-28 12:58:35 +0800

    Добавил UserViewSet

---
+++ api_yamdb/api/views.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import filters
+
+from users.serializers import UserSerializer
+from .permission import IsAdmin
+
+User = get_user_model()
+
+
+class UserViewSet(ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    lookup_field = 'username'
+    http_method_names = ['get', 'post', 'patch', 'delete',
+                         'head', 'options', 'trace']
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=username',)
+
+    @action(detail=False, methods=['GET', 'PATCH'], url_path='me')
+    def me(self, request):
+        if request.method == 'GET':
+            serializer = self.serializer_class(request.user)
+            return Response(serializer.data)
+        if request.method == 'PATCH':
+            serializer = self.serializer_class(
+                instance=request.user,
+                data=request.POST,
+                partial=True
+            )
+            serializer.fields.pop('role')
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def get_permissions(self):
+        if self.action == 'me':
+            permission_classes = (IsAuthenticated,)
+        else:
+            permission_classes = (IsAdmin,)
+        return [permission() for permission in permission_classes]
