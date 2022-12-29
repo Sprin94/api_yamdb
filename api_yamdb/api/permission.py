e00f71949a0ae62d59a6d5523e204c6634e7d263
Sprin <Electron4ik123@gmai.com>
2022-12-29 08:59:47 +0800

    Убрал лишний класс прав доступа

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -5,14 +5,6 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
-        return obj.author == request.user
-
-
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
         return ((request.user.is_authenticated
