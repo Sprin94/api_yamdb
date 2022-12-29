71222b27c674bdee7c3c3f1122ef5657bba7b497
Yana-VV <112080170+Yana-VV@users.noreply.github.com>
2022-12-29 17:43:02 +0500

    Update permission.py

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -5,5 +5,5 @@ class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
-                or request.user.Isadmin
-                or request.user.Ismoderator)
+                or request.user.role == User.RoleChoice.ADMIN
+                or request.user.role == User.RoleChoice.MODERATOR)
