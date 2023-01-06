2d29a8bc0e4c9cd924a5b3f91cdba704a5801df9
Яна <yuki122@yandex.ru>
2023-01-06 12:38:19 +0500

    Добавлена проверка на авторизацию

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -3,12 +3,18 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
                 or request.user.role == User.RoleChoice.ADMIN
                 or request.user.role == User.RoleChoice.MODERATOR)
+
+    def has_permission(self, request, view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+
 
 class IsAdmin(permissions.BasePermission):
     def has_permission(self, request, view):
@@ -22,11 +28,3 @@ class IsModerator(permissions.BasePermission):
         return ((request.user.is_authenticated
                 and request.user.role == User.RoleChoice.MODERATOR)
                 or request.user.is_superuser)
-
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
-
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated)
